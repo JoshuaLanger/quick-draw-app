235501f486ddef65d67cb7f0d750304413d81265
unknown <joshuarllanger@gmail.com>
2018-08-23 21:47:33 -0500

    updated button

--- src/styled-components/ButtonStyle.js
+++ src/styled-components/ButtonStyle.js
@@ -2,13 +2,24 @@ import styled from 'styled-components';
 import woodTexture from '../assets/wood-texture.svg';
 
 const ButtonStyle = styled.button`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
   border: none;
   background: #e5b85c url(${woodTexture});
   font-size: 1.2em;
   font-family: 'Cabin', 'Arial', sans-serif;
   padding: 1em 2em;
   cursor: pointer;
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.3);
+
+  @media (min-width: 500px) {
+    position: static;
+    display: inline-block;
+    max-width: 20rem;
+    margin: 3rem;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
+  }
 `;
 
 export default ButtonStyle;
