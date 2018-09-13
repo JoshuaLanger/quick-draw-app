f167bb1f0e46925880cda78b83bd9ec4f9780e72
Joshua Langer <joshuarllanger@gmail.com>
2018-09-13 11:36:41 -0500

    updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,46 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import woodTexture from '../assets/wood-texture.svg';
 
-const TitleStyle = styled.header`
+const HeaderStyle = styled.header`
+  /* Mobile-first */
+  padding: 0.625rem 3.75rem;
   background: #e5b85c url(${woodTexture});
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.3);
-  margin-bottom: 4em;
+  box-shadow: 0 0.25rem 0.25rem rgba(0, 0, 0, 0.3);
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
 
-  @media (max-width: 425px) {
-    flex-direction: column;
+  @media (min-width: 640px) {
+    padding: 0.625rem 20%;
+    flex-direction: row;
+    justify-content: space-between;
   }
+`;
+
+const Brand = styled.h1`
+  width: 240px;
+  font-size: 2.25rem;
+  font-family: 'Rye', sans-serif;
+`;
+
+const Info = styled.div`
+  width: 240px;
+  font-weight: normal;
+  display: flex;
+  justify-content: space-between;
 `;
 
 class Header extends Component {
   render() {
     const { score } = this.props;
     return (
-      <TitleStyle>
-        <h1 style={{ fontFamily: 'Rye' }}>Quick Draw!</h1>
-        <h2>Score: {score}</h2>
-        <h2>Time: 0:00</h2>
-      </TitleStyle>
+      <HeaderStyle>
+        <Brand>Quick Draw!</Brand>
+        <Info>
+          <h2>Score: {score} </h2>
+          <h2>Time: 0:00</h2>
+        </Info>
+      </HeaderStyle>
     );
   }
 }
