6ff7404690f88f7e3b709fae2a787c3914d9612b
Joshua Langer <joshuarllanger@gmail.com>
2019-02-07 10:28:30 -0600

    added default theme

---
+++ src/themes/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+    colors: {
+        'oak-1': '#332200',
+        'oak-2': '#B8800F',
+        'oak-1': '#EBB548',
+        'oak-1': '#EFCF8F',
+        'oak-1': '#F5EBD6',
+        'red':   '#E83138',
+        'blue':  '#297BA3',
+        'green': '#2CB45A'
+    },
+    fontFamily: {
+        'title': "'Rye', serif",
+        'head': "'Cabin', sans-serif",
+        'body': "'Open Sans', sans-serif"
+    }
+}
+
+export default theme;
