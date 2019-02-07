32d0e9314b3787d96fdf4bc95f07d03f7bc386bd
Joshua Langer <joshuarllanger@gmail.com>
2019-02-07 15:43:50 -0600

    refactored Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,31 +2,19 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import woodTexture from '../assets/wood-texture.svg';
 
-const ButtonStyle = styled.button`
-  position: absolute;
-  bottom: 0;
-  left: 0;
+const StyledButton = styled.button`
+  height: 8rem;
   width: 100%;
   border: none;
-  background: #e5b85c url(${woodTexture});
-  font-size: 1.2em;
-  font-family: 'Cabin', 'Arial', sans-serif;
-  padding: 1em 2em;
+  background: ${props => props.theme.colors['oak-3']};
+  text-align: center;
   cursor: pointer;
-
-  @media (min-width: 500px) {
-    position: static;
-    display: inline-block;
-    max-width: 20rem;
-    margin: 3rem;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
-  }
 `;
 
 class Button extends Component {
   render() {
     const { message } = this.props;
-    return <ButtonStyle>{message}</ButtonStyle>;
+    return <StyledButton>{message}</StyledButton>;
   }
 }
 
