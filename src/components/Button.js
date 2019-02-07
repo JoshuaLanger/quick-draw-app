e77be3778c6f58b5593b39aac332d90cd20e46dc
Joshua Langer <joshuarllanger@gmail.com>
2019-02-07 15:54:41 -0600

    linked Heading4 to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import woodTexture from '../assets/wood-texture.svg';
+
+import { Heading4 } from './Font';
+
+// have two button styles: "bar" and "icon"
 
 const StyledButton = styled.button`
   height: 8rem;
@@ -14,8 +19,20 @@ const StyledButton = styled.button`
 class Button extends Component {
   render() {
     const { message } = this.props;
-    return <StyledButton>{message}</StyledButton>;
+    return (
+      <StyledButton>
+        <Heading4>{message}</Heading4>
+      </StyledButton>
+    );
   }
 }
+
+Button.defaultProps = {
+  message: 'START'
+};
+
+Button.propTypes = {
+  message: PropTypes.string
+};
 
 export default Button;
