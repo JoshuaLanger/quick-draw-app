2f956233b83f2fdebdd916263c8377e64e4b43a4
Joshua Langer <joshuarllanger@gmail.com>
2019-02-07 13:00:39 -0600

    fixed theme

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -2,9 +2,9 @@ const theme = {
     colors: {
         'oak-1': '#332200',
         'oak-2': '#B8800F',
-        'oak-1': '#EBB548',
-        'oak-1': '#EFCF8F',
-        'oak-1': '#F5EBD6',
+        'oak-3': '#EBB548',
+        'oak-4': '#EFCF8F',
+        'oak-5': '#F5EBD6',
         'red':   '#E83138',
         'blue':  '#297BA3',
         'green': '#2CB45A'
