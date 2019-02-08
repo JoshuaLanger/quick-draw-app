b9b91114d7d34b840db8df7de278daf14a7ffdc5
Joshua Langer <joshuarllanger@gmail.com>
2019-02-08 10:38:00 -0600

    adjusted Font classes

--- src/components/Font.js
+++ src/components/Font.js
@@ -2,28 +2,38 @@ import styled from 'styled-components';
 
 const Title = styled.h1`
   font-size: 4rem;
-  font-weight: 500;
+  line-height: 1.5;
+  letter-spacing: 1.6px;
+  font-weight: 400;
   font-family: ${props => props.theme.fontFamily['title']};
   text-align: center;
 `;
 
 const Heading2 = styled.h2`
   font-size: 3.6rem;
+  line-height: 1.5;
+  letter-spacing: 1.6px;
   font-family: ${props => props.theme.fontFamily['head']};
 `;
 
 const Heading3 = styled.h2`
   font-size: 3rem;
+  line-height: 1.5;
+  letter-spacing: 1.6px;
   font-family: ${props => props.theme.fontFamily['head']};
 `;
 
 const Heading4 = styled.h4`
+  line-height: 1.3;
   font-size: 2.4rem;
+  letter-spacing: 1.6px;
   font-family: ${props => props.theme.fontFamily['head']};
 `;
 
-const Paragraph = styled.h4`
+const Paragraph = styled.p`
+  margin-bottom: 1rem;
   font-size: 1.6rem;
+  line-height: 1.2;
   font-family: ${props => props.theme.fontFamily['body']};
 `;
 
